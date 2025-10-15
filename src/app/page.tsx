55fa2d4f3eadba11e4700f58994b595856e724f7
r.organ <r.organ@dan-it.com.ua>
2025-10-15 06:37:35 +0300

    db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  const users = await prisma.user.findMany();
   return (
     <div className="text-red-500">
-      Hello
+      {JSON.stringify(users)}
       <Button>Click me</Button>
     </div>
   );
