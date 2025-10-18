7a22487d6c9b956772b42fd1bbad353cd5cf1ce7
r.organ <r.organ@dan-it.com.ua>
2025-10-18 23:07:23 +0300

    inngest setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,33 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { requireAuth } from "@/lib/auth-utils";
+import { useTRPC } from "@/trpc/client";
 import { caller } from "@/trpc/server";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
-export default async function Home() {
-  await requireAuth();
+export default function Home() {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  console.log("🚀 ~ data:", data);
 
-  const data = await caller.getUsers();
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        toast.success("Job queued");
+      },
+    })
+  );
+
   return (
     <div className="">
       protected server component
       {JSON.stringify(data)}
-      <Button>Click me</Button>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create workflow
+      </Button>
     </div>
   );
 }
+``;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,13 +1,19 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { inngest } from "@/inngest/client";
+import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
     console.log({ userId: ctx.auth.user.id });
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id,
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "roman@gmail.com",
       },
     });
+    return { success: true, message: "Job queued" };
   }),
 });
 // export type definition of API
