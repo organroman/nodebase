24311cc6edb93391b5ce8134d6036e87c0e41613
r.organ <r.organ@dan-it.com.ua>
2025-11-07 09:29:53 +0200

    workflows crud

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,10 +1,29 @@
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import {
+  WorkflowsContainer,
+  WorkflowsList,
+} from "@/features/workflows/components/worfloflows";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
+
 import { requireAuth } from "@/lib/auth-utils";
-import React from "react";
+import { HydrateClient } from "@/trpc/server";
 
 const WorkflowPage = async () => {
   await requireAuth();
-  
-  return <div>workflow page</div>;
+  prefetchWorkflows();
+
+  return (
+    <WorkflowsContainer>
+      <HydrateClient>
+        <ErrorBoundary fallback={<p>Error!</p>}></ErrorBoundary>
+        <Suspense fallback={<p>Loading</p>}>
+          <WorkflowsList />
+        </Suspense>
+      </HydrateClient>
+    </WorkflowsContainer>
+  );
 };
 
 export default WorkflowPage;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,27 +1,9 @@
-import { inngest } from "@/inngest/client";
-import { createTRPCRouter, protectedProcedure } from "../init";
-import prisma from "@/lib/db";
+import { createTRPCRouter } from "../init";
+
+import { workflowsRouter } from "@/features/workflows/server/routers";
 
 export const appRouter = createTRPCRouter({
-  testAi: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "execute/ai",
-    });
-    return { success: true, message: "Job queued" };
-  }),
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
-    console.log({ userId: ctx.auth.user.id });
-    return prisma.workflow.findMany();
-  }),
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: {
-        email: "roman@gmail.com",
-      },
-    });
-    return { success: true, message: "Job queued" };
-  }),
+  workflows: workflowsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
